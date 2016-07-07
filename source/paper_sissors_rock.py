b84ea1da0d9d855efa5ec826d66fbb40064b63f9
jon_norman <jon.norman@theguardian.com>
2016-07-07 16:55:44 +0100

    added some more friendly variables names and fixed the problem of drawing a game and getting "<choice> wins!"

--- source/paper_sissors_rock.py
+++ source/paper_sissors_rock.py
@@ -1,34 +1,34 @@
 import random
 
 choices = ['paper', 'sissors', 'rock']
-chosen = random.choice(choices)
+computer_choice = random.choice(choices)
 
 while True:
     msg = 'Type one of following {}: '.format(' '.join(choices))
-    usr = input(msg)
-    if usr in choices:
+    user_choice = input(msg)
+    if user_choice in choices:
         break
     print('Please choose a correct choice')
 
-print('computer choses: {}'.format(chosen))
+print('computer chooses: {}'.format(computer_choice))
 
-if usr == chosen:
+if user_choice == computer_choice:
     print('The result is a tie!')
+else:
+    if user_choice == 'paper':
+        if computer_choice == 'rock':
+            print('paper wins')
+        else:
+            print('rock wins')
 
-if usr == 'paper':
-    if chosen == 'rock':
-        print('paper wins')
-    else:
-        print('rock wins')
+    if user_choice == 'sissors':
+        if computer_choice == 'paper':
+            print('sissors wins')
+        else:
+            print('rock wins')
 
-if usr == 'sissors':
-    if chosen == 'paper':
-        print('sissors wins')
-    else:
-        print('rock wins')
-
-if usr == 'rock':
-    if chosen == 'sissors':
-        print('rock wins')
-    else:
-        print('paper wins')
+    if user_choice == 'rock':
+        if computer_choice == 'sissors':
+            print('rock wins')
+        else:
+            print('paper wins')
